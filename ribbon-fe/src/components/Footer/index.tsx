fc1dccb857702609a540bea3045e5256f185d1ad
daniel kwok <danielzhengxian21@gmail.com>
2022-03-07 10:20:39 +0800

    combined all css into index.css 2

--- ribbon-fe/src/components/Footer/index.tsx
+++ ribbon-fe/src/components/Footer/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import './index.css'
 import Typography from '@mui/material/Typography';
 import HomeIcon from '@mui/icons-material/Home';
 import { useNavigate } from 'react-router-dom';
